bc407c90e04cb2685c03e491610647bb672dd617
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-09-16 21:01:34 +0500

    Added a animator controller for the behaviour system

---
+++ ActorAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using ToolBox.Attributes;
+
+public class ActorAnimator : MonoBehaviour
+{
+	public bool animationState { get; private set; }
+
+	[SerializeField, BoxGroup("Components")] private Animator animator = null;
+	[SerializeField, BoxGroup("Components")] private BehaviourProcessor behaviorProcessor = null;
+
+	private int animatorParametr = 0;
+	private int subAnimatorParametr = 0;
+
+	private void Start()
+	{
+		animatorParametr = Animator.StringToHash("State");
+		subAnimatorParametr = Animator.StringToHash("Blend");
+
+		behaviorProcessor.onStateChange += UpdateAnimator;
+		enabled = behaviorProcessor.enabled == true && behaviorProcessor != null;
+	}
+	
+	private void UpdateAnimator()
+	{
+		animator.SetInteger(animatorParametr, behaviorProcessor.currentState.stateIndex);
+		animationState = false;
+	}
+
+	// Called by an animation event
+	public void SetAnimationState(bool animationState) => this.animationState = animationState;
+
+	// Called by an animation event
+	public void PlayerRandomSubAnimation() => animator.SetFloat(subAnimatorParametr, Random.value);
+}
