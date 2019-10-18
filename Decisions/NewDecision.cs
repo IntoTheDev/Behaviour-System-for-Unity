5cef08224ce9e19a682ca2eeb2e5a595355aa2ba
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-10-18 17:39:40 +0500

    Update

---
+++ Decisions/NewDecision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ToolBox.Behaviours
+{
+	public class NewDecision : Decision
+	{
+		public override void EnterDecision()
+		{
+
+		}
+
+		public override bool Decide()
+		{
+			return true;
+		}
+
+		public override void ExitDecision()
+		{
+
+		}
+	}
+}
+
