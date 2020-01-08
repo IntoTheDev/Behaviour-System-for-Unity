a83b9006e758bff20d1daddb8db7101dc680b52b
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-01-08 19:57:02 +0500

    Update Wait.cs

--- Conditions/Common/Wait.cs
+++ Conditions/Common/Wait.cs
@@ -2,12 +2,6 @@
 {
 	public class Wait : Condition
 	{
-		public override void OnEnter() =>
-			RunTask();
-
-		public override void OnExit() =>
-			StopTask();
-
 		protected override void Task() =>
 			ProcessCondition(true);
 	}
